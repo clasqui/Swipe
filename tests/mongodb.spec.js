8f11c2120cd49ef52a8015fa88c35de8e6a714ce
Marc Clasca <hulehule20@gmail.com>
2014-04-08 13:16:02 +0200

    Added more extense tests on MongoDB (insertint, quering and deleting in users collection)

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -7,4 +7,68 @@ describe("MongoDB", function() {
 			next();
 		});
 	});
+	it("should populate db", function(next) {
+		var MongoClient = require('mongodb').MongoClient;
+		MongoClient.connect('mongodb://127.0.0.1:27017/swipe', function(err, db) {
+			
+			db.collection('users', function(err, collection){
+				expect(err).toBe(null);
+				expect(collection).toBeDefined();
+				doc = {
+					key: 'testing',
+					value1: 1320,
+					value2: ['Marc', 'Clasca', 'Ramirez']
+				}
+
+				collection.save(doc, function(err, record){
+					expect(err).toBe(null);
+					next();
+				});
+			})
+
+		});
+	});
+
+	it("should get values", function(next) {
+		var MongoClient = require('mongodb').MongoClient;
+		MongoClient.connect('mongodb://127.0.0.1:27017/swipe', function(err, db) {
+			
+			db.collection('users', function(err, collection){
+				
+
+				collection.findOne({key: 'testing'}, function(err, document){
+					expect(err).toBe(null);
+
+					expect(document.value1).toBe(1320);
+					var cognom = document.value2[1];
+					expect(cognom).toBe('Clasca');
+					next();
+
+				});
+			})
+
+		});
+	});
+
+
+	it("should delete document", function(next) {
+		var MongoClient = require('mongodb').MongoClient;
+		MongoClient.connect('mongodb://127.0.0.1:27017/swipe', function(err, db) {
+			
+			db.collection('users', function(err, collection){
+				
+
+				collection.remove({key:'testing'}, function(err, numberOfRemovedDocs) {
+        			expect(err).toBe(null);
+        			expect(numberOfRemovedDocs).toBe(1);
+        			next();
+      			});
+
+			});
+
+		});
+	});
+
+
+
 });
